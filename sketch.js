145500a63118ca643b309d13276c3d305b488db0
matildeteani <matilde.teani@libero.it>
2019-11-27 11:43:58 +0100

    adding logo

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 let newCanvas;
 let themeSong;
+let logo;
+let volume = 0;
+let analyzer;
 
 function preload() {
-  themeSong = loadSound('./assets/TG1_new.mp3');
+  themeSong = loadSound("./assets/TG1_new.mp3");
+  logo = loadImage("./assets/TG1.png");
 }
 
 function setup() {
@@ -10,9 +14,12 @@ function setup() {
   newCanvas.mouseMoved(playTheme);
 
   rectMode(CENTER);
+  imageMode(CENTER);
 
   analyzer = new p5.Amplitude();
   analyzer.setInput(themeSong);
+
+  showReplayButton();
 }
 
 function draw() {
@@ -20,7 +27,7 @@ function draw() {
   
   tg1Logo();
 
-  showReplayButton();
+  //showReplayButton();
 }
 
 function windowResized() {
@@ -35,17 +42,18 @@ function playTheme() {
 }
 
 function tg1Logo() {
-  let volume = 0;
   volume = analyzer.getLevel();
-  volume = map(volume, 0, 1, 50, 255);
+  volume = map(volume, 0, 1, 0, width / 2);
 
-  fill(52, 64, 100);
+  //console.log("var volume:" + volume);
+
+  fill(255, 233, 92);
   noStroke();
-  rect(windowWidth / 4, windowHeight / 2, 50, 50);
+  image(logo, windowWidth / 4, windowHeight / 2, logo.width / 10 + volume, logo.height / 10 + volume);
 }
 
 function showReplayButton() {
-  if (newCanvas.mouseMoved(false)) {
+  if (themeSong.isPlaying() == false) {
     replayButton();
   }
 }
@@ -53,5 +61,6 @@ function showReplayButton() {
 function replayButton() {
   let playButton = createButton('Replay TG1 theme');
   playButton.position(windowWidth / 4, (windowHeight / 4) * 3);
+  playButton.addClass("button");
   playButton.mousePressed(playTheme);
 }
